ab0e0234e9619859e8c61863aa1d855d672f206f
Dinesh-Talwadker <talwadkerdinesh@gmail.com>
2022-07-23 18:04:09 +0530

    updated routes

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,16 @@
 const express = require("express")
+const path = require("path")
 const app = express()
 
-app.get("/", (req,res)=>{
-    res.render("../frontend/")
-})
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+
+// routes
+const MainRouter = require("../backend/routes/main")
+
+app.use("/", MainRouter)
+
+app.use(express.static(path.join(__dirname)));
+app.use(express.static('../frontend/'));
 
 app.listen(8080, ()=> console.log("server started"))
--- backend/routes/main.js
+++ backend/routes/main.js
@@ -1,4 +1,8 @@
 const express = require("express")
 const Router = express()
 
+app.get("/", (req,res)=>{
+    res.render("../frontend/")
+})
+
 module.exports = Router
