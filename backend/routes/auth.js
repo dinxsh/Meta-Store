c6a624ee5c3b8256f51635ffca6c5a475d160e2f
Dinesh-Talwadker <talwadkerdinesh@gmail.com>
2022-08-03 21:26:09 +0530

    🏠 updated home page

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,17 +1,45 @@
 const express = require("express")
 const Router = express()
 
-Router.get("/register", (req,res)=>{
-    res.json({status:200, info:"registered acc", registeredwith:"google"})
-})
-
-Router.get("/login", (req,res)=>{
-    res.json({status:200, info:"logged in acc", logwith:"google"})
-})
-
-Router.get("/forgotpassword", (req,res)=>{
-    res.json({status:200, info:"changed password"})
-})
+Router.get("/register", function (req, res) {
+    res.render("register");
+});
+ 
+// Handling user signup
+Router.post("/register", function (req, res) {
+    var username = req.body.username
+    var password = req.body.password
+    User.register(new User({ username: username }),
+            password, function (err, user) {
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        }
+ 
+        passport.authenticate("local")(
+            req, res, function () {
+            res.render("secret");
+        });
+    });
+});
+ 
+//Showing login form
+Router.get("/login", function (req, res) {
+    res.render("login");
+});
+ 
+//Handling user login
+Router.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), function (req, res) {
+});
+ 
+//Handling user logout
+Router.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
+});
 
 // Temp data filled in
 
